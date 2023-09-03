7768ab4493090e663097aaef04ac16ee1d5bdc99
Jeong seulho <wjdtmfgh@gmail.com>
2023-09-03 19:41:11 +0900

    Design: skill design refactor

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.querySelector('.go-education').addEventListener('click', function () {
   const aboutSectionTopLocation =
     document.querySelector('.education').offsetTop;
   window.scrollTo({
-    top: aboutSectionTopLocation - 50,
+    top: aboutSectionTopLocation,
     behavior: 'smooth',
   });
 });
@@ -18,7 +18,7 @@ document.querySelector('.go-education').addEventListener('click', function () {
 document.querySelector('.go-skill').addEventListener('click', function () {
   const aboutSectionTopLocation = document.querySelector('.skill').offsetTop;
   window.scrollTo({
-    top: aboutSectionTopLocation - 50,
+    top: aboutSectionTopLocation,
     behavior: 'smooth',
   });
 });
@@ -26,7 +26,7 @@ document.querySelector('.go-skill').addEventListener('click', function () {
 document.querySelector('.go-project').addEventListener('click', function () {
   const aboutSectionTopLocation = document.querySelector('.project').offsetTop;
   window.scrollTo({
-    top: aboutSectionTopLocation - 50,
+    top: aboutSectionTopLocation,
     behavior: 'smooth',
   });
 });
@@ -38,46 +38,6 @@ document.querySelector('.go-blog').addEventListener('click', function () {
     behavior: 'smooth',
   });
 });
-
-document.querySelector('.tab-menu').addEventListener('click', function (e) {
-  if (e.target.tagName.toLowerCase() === 'img') {
-    const parentDiv = e.target.closest('.img-wrapper');
-    const index = Array.from(
-      e.currentTarget.querySelectorAll('.img-wrapper'),
-    ).indexOf(parentDiv);
-
-    // 화살표 아이콘의 위치를 변경
-    const arrowIcon = document.querySelector('.arrow-icon');
-    const targetTop =
-      parentDiv.offsetTop +
-      parentDiv.offsetHeight / 2 -
-      arrowIcon.offsetHeight / 2;
-    arrowIcon.style.top = `${targetTop}px`;
-
-    showTab(index);
-  }
-});
-
-function showTab(index) {
-  const panels = document.querySelectorAll('.tab-panel');
-  const imgWrapper = document.querySelectorAll('.tab-menu .img-wrapper');
-  const images = document.querySelectorAll('.tab-menu img-wrapper img');
-
-  panels.forEach((panel, i) => {
-    if (i === index) panel.classList.add('active');
-    else panel.classList.remove('active');
-  });
-
-  imgWrapper.forEach((img, i) => {
-    if (i === index) img.classList.add('active');
-    else img.classList.remove('active');
-  });
-
-  images.forEach((img, i) => {
-    if (i === index) img.classList.add('active');
-    else img.classList.remove('active');
-  });
-}
 
 let slideContainer = document.getElementById('slide-inner');
 let slideItems = document.querySelectorAll('.slide-item'); // ".slide-item"으로 변경
